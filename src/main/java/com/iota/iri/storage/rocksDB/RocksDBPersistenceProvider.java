ae1ce0ca3c35822d4bdfdd505632c4d808621e6b
Paul D Handy <paul.d.handy@gmail.com>
2017-05-08 12:38:02 -0600

    commented out background thread limit for RocksEnv

--- src/main/java/com/iota/iri/storage/rocksDB/RocksDBPersistenceProvider.java
+++ src/main/java/com/iota/iri/storage/rocksDB/RocksDBPersistenceProvider.java
@@ -364,9 +364,11 @@ public class RocksDBPersistenceProvider implements PersistenceProvider {
             throw e;
         }
 
+        /*
         RocksEnv.getDefault().setBackgroundThreads(Runtime.getRuntime().availableProcessors())
                 .setBackgroundThreads(Runtime.getRuntime().availableProcessors(), RocksEnv.FLUSH_POOL)
                 .setBackgroundThreads(Runtime.getRuntime().availableProcessors(), RocksEnv.COMPACTION_POOL);
+                */
 
         bloomFilter = new BloomFilter(BLOOM_FILTER_BITS_PER_KEY);
         BlockBasedTableConfig blockBasedTableConfig = new BlockBasedTableConfig().setFilter(bloomFilter);
